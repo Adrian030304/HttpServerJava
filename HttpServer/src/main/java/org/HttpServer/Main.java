ba9dc216e85abb9b3dc61c6d6f4ca3e91d2f685b
Adrian030304 <adrianromanenco1@gmail.com>
2026-02-27 23:39:07 +0200

    Created a network socket that accepts connections on a TCP port.

--- HttpServer/src/main/java/org/HttpServer/Main.java
+++ HttpServer/src/main/java/org/HttpServer/Main.java
@@ -1,7 +1,12 @@
 package org.HttpServer;
 
-public class Main {
-    public static void main(String[] args) {
+import java.net.ServerSocket;
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        final int PORT = 8080;
+        final ServerSocket server = new ServerSocket(PORT);
+        System.out.println("Server is listening to port: " + PORT);
+        server.close();
     }
 }
