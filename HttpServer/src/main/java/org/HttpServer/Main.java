7d88779e2127a434e7adf9a585bd1183b5de793e
Adrian030304 <adrianromanenco1@gmail.com>
2026-02-27 23:51:22 +0200

    Applied a blocking method which blocks the current thread until a connection is made toward the server.

--- HttpServer/src/main/java/org/HttpServer/Main.java
+++ HttpServer/src/main/java/org/HttpServer/Main.java
@@ -1,12 +1,18 @@
 package org.HttpServer;
 
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         final int PORT = 8080;
         final ServerSocket server = new ServerSocket(PORT);
         System.out.println("Server is listening to port: " + PORT);
-        server.close();
+        while (true) {
+            final Socket client = server.accept();
+
+            server.close();
+        }
+
     }
 }
